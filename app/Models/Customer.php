26c9fce51118faf26c7033e278bff7932df1deff
Irving Zamora <irvingzamora28@gmail.com>
2022-04-06 20:16:00 -0600

    Add Customer Controller and Resource

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'status' => 'A',
+    ];
+
 }
